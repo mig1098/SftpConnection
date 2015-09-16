68b0b413f202adfe34150e0d91e953d8a9d7033b
mig1098 <mig1098@hotmail.com>
2015-09-16 17:08:22 -0500

    upload1

---
+++ phpseclib/phpsec.php
@@ -0,0 +1,29 @@
+<?php
+require_once BASE_APPPATH.'Library/phpseclib/Crypt/Base.php';
+require_once BASE_APPPATH.'Library/phpseclib/Crypt/Blowfish.php';
+require_once BASE_APPPATH.'Library/phpseclib/Crypt/DES.php';
+require_once BASE_APPPATH.'Library/phpseclib/Crypt/Hash.php';
+require_once BASE_APPPATH.'Library/phpseclib/Crypt/Random.php';
+require_once BASE_APPPATH.'Library/phpseclib/Crypt/RC2.php';
+require_once BASE_APPPATH.'Library/phpseclib/Crypt/RC4.php';
+require_once BASE_APPPATH.'Library/phpseclib/Crypt/Rijndael.php';
+require_once BASE_APPPATH.'Library/phpseclib/Crypt/RSA.php';
+require_once BASE_APPPATH.'Library/phpseclib/Crypt/TripleDES.php';
+require_once BASE_APPPATH.'Library/phpseclib/Crypt/Twofish.php';
+require_once BASE_APPPATH.'Library/phpseclib/Crypt/AES.php';
+
+require_once BASE_APPPATH.'Library/phpseclib/File/ANSI.php';
+require_once BASE_APPPATH.'Library/phpseclib/File/ASN1.php';
+require_once BASE_APPPATH.'Library/phpseclib/File/X509.php';
+require_once BASE_APPPATH.'Library/phpseclib/File/ASN1/Element.php';
+
+require_once BASE_APPPATH.'Library/phpseclib/Math/BigInteger.php';
+
+require_once BASE_APPPATH.'Library/phpseclib/Net/SCP.php';
+require_once BASE_APPPATH.'Library/phpseclib/Net/SSH1.php';
+require_once BASE_APPPATH.'Library/phpseclib/Net/SSH2.php';
+require_once BASE_APPPATH.'Library/phpseclib/Net/SFTP/Stream.php';
+require_once BASE_APPPATH.'Library/phpseclib/Net/SFTP.php';
+
+require_once BASE_APPPATH.'Library/phpseclib/System/SSH/Agent.php';
+require_once BASE_APPPATH.'Library/phpseclib/System/SSH/Agent/Identity.php';
